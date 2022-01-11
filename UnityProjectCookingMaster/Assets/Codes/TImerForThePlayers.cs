1ead8bbc4548953bdc7b4208f4ec589f4f5a4a8f
ArisseSulemane <97555276+ArisseSulemane@users.noreply.github.com>
2022-01-11 21:24:12 +0000

    PlayersTimeCode

--- UnityProjectCookingMaster/Assets/Codes/TImerForThePlayers.cs
+++ UnityProjectCookingMaster/Assets/Codes/TImerForThePlayers.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class TImerForThePlayers : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    //variable of time 
+    static float timePlayerOne,timePlayerTwo;
+    static int timePlayerOne1,timePlayerTwo1;
+    //variable of IU element;
+    public Text timePlayerOneUI,timePlayerTwoUI;
+    void Start(){
+    //initializing time of player one
+    timePlayerOne=150; 
+    timePlayerTwo=150; 
+    }
     void Update()
     {
-        
+    //decreasing time for the player1;
+     timePlayerOne = timePlayerOne - 1 * Mathf.RoundToInt(Time.deltaTime);
+      //converting float decimal to in show in UI
+     timePlayerOne1=(int)timePlayerOne;  
+     timePlayerOneUI.text="Time: "+ timePlayerOne1 +"S";
+     //-------------------------------------------------
+      //decreasing time for the player2;
+     timePlayerTwo = timePlayerOne  -= Time.deltaTime;
+     //converting float decimal to in show in UI
+     timePlayerTwo1=(int)timePlayerTwo;   
+     timePlayerTwoUI.text="Time: "+ timePlayerTwo1 +"S";
     }
 }
